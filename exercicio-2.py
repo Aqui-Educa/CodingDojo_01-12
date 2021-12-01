84f7c413cac359267faf78f070e6f5bfd818476f
Livia Moreira <livia.moreira222@gmail.com>
2021-12-01 20:53:08 -0300

    Continuação exerc. 02

--- exercicio-2.py
+++ exercicio-2.py
@@ -1,6 +1,6 @@
 # Uma fruteira está vendendo frutas com a seguinte tabela de preços:
 
-#       Até 5 Kg           Acima de 5 Kg
+#                           Até 5 Kg           Acima de 5 Kg
 #     Morango         R$ 2,50 por Kg          R$ 2,20 por Kg
 #     Maçã            R$ 1,80 por Kg          R$ 1,50 por Kg
 # Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra 
@@ -9,6 +9,20 @@
 # (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente. 
 
 peso_maca = float(input("Informe peso da maça: "))
-peso_morango = float(input("Informe peso da morango: "))
-if peso < 5: 
-    print("")
+peso_morango = float(input("Informe peso do morango: "))
+valor_maca = 0
+valor_morango = 0
+
+if peso_maca <= 5: 
+    print(f'O valor a ser pago é R${peso_maca*1.80}')
+    valor_maca = peso_maca*1.80
+elif (peso_maca > 5):
+    print(f'O valor a ser pago é R${peso_maca*1.50}')
+    valor_maca = peso_maca*1.50
+
+if peso_morango <= 5:
+    print(f'O valor a ser pago é R${peso_morango*2.50}')
+    valor_morango = peso_morango*2.50
+elif peso_morango > 5:
+    print(f'O valor a ser pago é R${peso_morango*2.20}')
+    valor_morango = peso_morango*2.20
